814518885482a11e845a8a02a5120f7c296ecc10
DanielShamir93 <daniel.shamir.93@gmail.com>
2022-01-28 00:52:53 +0200

    deploy

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,11 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const MONGODB_URL = 'mongodb://localhost:27017/1K4A-Password';
-
-if (process.env.NODE_ENV === "production") {
-  MONGODB_URL = process.env.MONGODB_URL;
-}
+const MONGODB_URL = process.env.MONGODB_URL;
 
 mongoose.connect(
   MONGODB_URL, 
