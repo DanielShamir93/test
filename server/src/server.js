97680b8c96da5cc9f7a96a4691d3b37883435238
DanielShamir93 <daniel.shamir.93@gmail.com>
2022-01-27 11:17:50 +0200

    1

---
+++ server/src/server.js
@@ -0,0 +1,28 @@
+// require("./db/mongoose");
+// const usersRouter = require("./routes/users/users.router");
+// const accountsRouter = require("./routes/accounts/accounts.router");
+const express = require("express");
+const cors = require("cors");
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+const publicPath = path.join(__dirname, '../..', 'client/build');
+
+app.use(express.static(publicPath));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+// Routers
+// app.use("/users", usersRouter);
+// app.use("/accounts", accountsRouter);
+
+app.all('*', (req, res) => {
+  res.sendFile(path.resolve(publicPath, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at ${PORT}`);
+});
